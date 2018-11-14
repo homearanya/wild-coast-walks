4d331112c3e5e5c37942c306504644d5f72c1c6f
Carlos Gonzalez <homearanya@gmail.com>
2018-11-14 20:54:51 +0200

    fix spinner behaviour

--- src/components/SliderArea.js
+++ src/components/SliderArea.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Slider from "react-slick";
 
 import MyContext from './Context'
@@ -24,56 +24,74 @@ const settings = {
   slidesToScroll: 1
 };
 
-export default function SliderArea() {
-  return (
-    <div className="slider-area">
-      <div className="preview-2">
-        <Slider {...settings}>
-          <div>
-            <MyContext.Consumer>
-              {({ data, set }) => (
-                <img src={slider_1} alt="" title="#slider-1-caption1"
-                  onLoad={() => set({ loadSpinner: false })}
-                />
-              )}
-            </MyContext.Consumer>
-            <div className="nivo-caption" style={{ display: 'block' }}>
-              <div className="banner-content slider-1">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="text-content">
-                        <h1 className="title1"><span style={{ color: '#0967B9' }}> WILD COAST</span> <span style={{ color: '#98F339' }}>SLACKPACKING</span></h1>
-                        <h2 className="sub-title">Take a Walk on the</h2>
-                        <h2 className="sub-title"><span>wild side</span></h2>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <img src={slider_2} alt="" title="#slider-1-caption1" />
-            <div className="nivo-caption" style={{ display: 'block' }}>
-              <div className="banner-content slider-1">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="text-content">
-                        <h1 className="title1"><span style={{ color: '#0967B9' }}>MIDLANDS</span> <span style={{ color: '#98F339' }}>WALKS & Cycling TOURS</span></h1>
-                        <h2 className="sub-title">Meandering</h2>
-                        <h2 className="sub-title">through the <span>Natal Midlands</span></h2>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Slider>
-      </div>
-    </div>
 
-  )
+export default class SliderArea extends Component {
+  constructor() {
+    super();
+    this.image = React.createRef();
+    this.set = null;
+  }
+
+  componentDidMount() {
+    const img = this.image.current;
+    if (img && img.complete) {
+      console.log('image complete')
+      this.set({ loadSpinner: false });
+    } else {
+      console.log('image no complete')
+      img.addEventListener("onLoad", this.set({ loadSpinner: false }))
+    }
+  }
+
+  render() {
+    return (
+      <div className="slider-area">
+        <div className="preview-2">
+          <Slider {...settings}>
+            <div>
+              <MyContext.Consumer>
+                {({ set }) => {
+                  this.set = set;
+                  return <img src={slider_1} ref={this.image} alt="" title="#slider-1-caption1" />
+                }}
+              </MyContext.Consumer>
+              <div className="nivo-caption" style={{ display: 'block' }}>
+                <div className="banner-content slider-1">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <div className="text-content">
+                          <h1 className="title1"><span style={{ color: '#0967B9' }}> WILD COAST</span> <span style={{ color: '#98F339' }}>SLACKPACKING</span></h1>
+                          <h2 className="sub-title">Take a Walk on the</h2>
+                          <h2 className="sub-title"><span>wild side</span></h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img src={slider_2} alt="" title="#slider-1-caption1" />
+              <div className="nivo-caption" style={{ display: 'block' }}>
+                <div className="banner-content slider-1">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <div className="text-content">
+                          <h1 className="title1"><span style={{ color: '#0967B9' }}>MIDLANDS</span> <span style={{ color: '#98F339' }}>WALKS & Cycling TOURS</span></h1>
+                          <h2 className="sub-title">Meandering</h2>
+                          <h2 className="sub-title">through the <span>Natal Midlands</span></h2>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    )
+  }
 }
