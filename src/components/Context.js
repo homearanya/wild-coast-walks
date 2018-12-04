1da2267701f6b4d6670059d5bda34769a3abc2da
Carlos Gonzalez <homearanya@gmail.com>
2018-12-04 23:38:44 +0200

    disable spinner

--- src/components/Context.js
+++ src/components/Context.js
@@ -3,7 +3,7 @@ import React from "react"
 const defaultContextValue = {
     data: {
         // set your initial data shape here
-        loadSpinner: true,
+        loadSpinner: false,
     },
     set: () => { },
 }
