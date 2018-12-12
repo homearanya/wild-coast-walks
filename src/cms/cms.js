493456ac78497dd7d42c1448aecf5372b56589cf
Carlos Gonzalez <homearanya@gmail.com>
2018-12-12 18:16:04 +0100

    testing custom widgets netlify cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
 import CMS from "netlify-cms";
 
-import { RelationBisControl, RelationBisPreview } from "./widgets/RelationBis";
+import RelationBisControl from "./widgets/RelationBisControl";
+import RelationBisPreview from "./widgets/RelationBisPreview";
 
 CMS.registerWidget("relationBis", RelationBisControl, RelationBisPreview);
--- src/cms/widgets/RelationBis.js
+++
@@ -1,2 +0,0 @@
-export RelationBisControl from "./RelationBisControl";
-export RelationBisPreview from "./RelationBisPreview";
