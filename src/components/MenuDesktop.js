c02688d29dc432e93705f7bd5e0daa712ffca03f
Carlos Gonzalez <homearanya@gmail.com>
2018-12-17 15:56:17 +0100

    SA Trails New Logo

--- src/components/MenuDesktop.js
+++ src/components/MenuDesktop.js
@@ -1,224 +1,244 @@
-import React, { Component } from 'react'
-import Sticky from 'react-sticky-el';
-import { Link } from "gatsby"
-import Img from 'gatsby-image'
+import React, { Component } from "react";
+import Sticky from "react-sticky-el";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
 
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import logo_white from '../assets/img/logo/logo-trails-white.png'
-import logo_color from '../assets/img/logo/logo-trails-color.png'
+import logo_white from "../assets/img/logo/trails-logo-white.png";
+import logo_color from "../assets/img/logo/trails-logo-color.png";
 
-const MenuBar = (props) => {
-    return (
-        <div className={props.menuClass}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="logo-menu-bg">
-                            <div className="row">
-                                <Logo />
-                                <MainMenu
-                                    tourMenuSections={props.tourMenuSections}
-                                    switches={props.switches} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+const MenuBar = props => {
+  return (
+    <div className={props.menuClass}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="logo-menu-bg">
+              <div className="row">
+                <Logo />
+                <MainMenu
+                  tourMenuSections={props.tourMenuSections}
+                  switches={props.switches}
+                />
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 const Logo = () => {
-    return (
-        <div className="col-md-4 col-sm-12">
-            <div className="logo">
-                <Link to="/">
-                    <img className="logo-white" src={logo_white} alt="Sa Adventure Trails logo"></img>
-                    <img className="logo-color" src={logo_color} alt="Sa Adventure Trails logo"></img>
-                </Link>
+  return (
+    <div className="col-md-4 col-sm-12">
+      <div className="logo">
+        <Link to="/">
+          <img
+            className="logo-white"
+            src={logo_white}
+            alt="Sa Adventure Trails logo"
+          />
+          <img
+            className="logo-color"
+            src={logo_color}
+            alt="Sa Adventure Trails logo"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const SubMenuDest = props => {
+  let imageFluid = "";
+  let imageAlt = "";
+  if (props.section.image) {
+    imageFluid = props.section.image.image.childImageSharp.fluid;
+    imageAlt = props.section.image.alt;
+  }
+  return (
+    <div>
+      <span className="border-hover">
+        <a className="mega-image">
+          <Img fluid={imageFluid} alt={imageAlt} />
+        </a>
+      </span>
+      <span>
+        <div className="mega-title">
+          <h4>
+            {props.section.heading1} <span>{props.section.heading2}</span>
+          </h4>
+        </div>
+        {props.section.tours.map((tour, index) => (
+          <Link
+            key={index}
+            to={tour.slug}
+            className="mega-item"
+            onClick={props.handleLeave}
+          >
+            {tour.tour + " (" + tour.duration + ")"}
+          </Link>
+        ))}
+      </span>
+    </div>
+  );
+};
+
+const SubMenu = props => {
+  return (
+    <div className="megamenu">
+      {props.tourMenuSections.map((section, index) => {
+        if (index % 2 === 0) {
+          return (
+            <div key={index} className="megamenu-list clearfix">
+              <SubMenuDest section={section} handleLeave={props.handleLeave} />
+              {props.tourMenuSections[index + 1] ? (
+                <SubMenuDest
+                  section={props.tourMenuSections[index + 1]}
+                  handleLeave={props.handleLeave}
+                />
+              ) : null}
             </div>
-        </div>
-    )
-}
-
-const SubMenuDest = (props) => {
-    let imageFluid = '';
-    let imageAlt = '';
-    if (props.section.image) {
-        imageFluid = props.section.image.image.childImageSharp.fluid;
-        imageAlt = props.section.image.alt;
-    }
-    return (
-        <div>
-            <span className="border-hover">
-                <a className="mega-image" >
-                    <Img fluid={imageFluid} alt={imageAlt} />
-                </a>
-            </span>
-            <span>
-                <div className="mega-title">
-                    <h4>{props.section.heading1} <span>{props.section.heading2}</span></h4>
-                </div>
-                {props.section.tours.map((tour, index) =>
-                    <Link
-                        key={index}
-                        to={tour.slug}
-                        className="mega-item"
-                        onClick={props.handleLeave}>
-                        {tour.tour + " (" + tour.duration + ")"}
-                    </Link>
-                )}
-            </span>
-        </div>
-    )
-}
-
-const SubMenu = (props) => {
-    return (
-        <div className="megamenu">
-            {props.tourMenuSections.map((section, index) => {
-                if (index % 2 === 0) {
-                    return <div key={index} className="megamenu-list clearfix">
-                        <SubMenuDest
-                            section={section}
-                            handleLeave={props.handleLeave}
-                        />
-                        {props.tourMenuSections[index + 1] ?
-                            <SubMenuDest
-                                section={props.tourMenuSections[index + 1]}
-                                handleLeave={props.handleLeave}
-                            />
-                            : null}
-                    </div>
-                } else {
-                    return null
-                }
-            })}
-        </div>
-    )
-}
+          );
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
+};
 
 class MainMenu extends Component {
-    constructor() {
-        super()
-        this.state = {
-            showSubMenu: false
-        };
-        this.handleHover = this.handleHover.bind(this);
-        this.handleLeave = this.handleLeave.bind(this);
-    }
-
-    handleHover = () => {
-        this.setState({ showSubMenu: true });
+  constructor() {
+    super();
+    this.state = {
+      showSubMenu: false
     };
+    this.handleHover = this.handleHover.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
+  }
 
-    handleLeave = () => {
-        this.setState({ showSubMenu: false });
-    };
+  handleHover = () => {
+    this.setState({ showSubMenu: true });
+  };
 
-    render() {
-        return (
-            <div className="col-md-8 hidden-sm hidden-xs">
-                <div className="mainmenu">
-                    <nav>
-                        <ul id="nav">
-                            <li>
-                                <Link
-                                    to="/" className="menu-item"
-                                    activeClassName="menu-item-active" >
-                                    HOME
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/about/"
-                                    className="menu-item"
-                                    activeClassName="menu-item-active">
-                                    ABOUT US
-                                </Link>
-                            </li>
-                            <li onMouseLeave={this.handleLeave} onMouseEnter={this.handleHover}>
-                                <a href="#"> Tours </a>
-                                <TransitionGroup>
-                                    <CSSTransition
-                                        classNames="fade"
-                                        timeout={{ enter: 500, exit: 300 }}
-                                    >
-                                        <div>
-                                            {this.state.showSubMenu && <SubMenu tourMenuSections={this.props.tourMenuSections} handleLeave={this.handleLeave} />}
-                                        </div>
-                                    </CSSTransition>
-                                </TransitionGroup>
-                            </li>
-                            {this.props.switches.calendarswitch ?
-                                <li>
-                                    <Link
-                                        to="/tour-calendar/"
-                                        className="menu-item"
-                                        activeClassName="menu-item-active">
-                                        Tour Calendar
-                                </Link>
-                                </li>
-                                :
-                                null
-                            }
-                            {this.props.switches.blogswitch ?
-                                <li>
-                                    <Link
-                                        to="/blog/"
-                                        className="menu-item"
-                                        activeClassName="menu-item-active">
-                                        BLOG
-                                </Link>
-                                </li>
-                                :
-                                null
-                            }
-                            <li>
-                                <Link
-                                    to="/contact/"
-                                    className="menu-item"
-                                    activeClassName="menu-item-active">
-                                    Contact Us
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        )
-    }
+  handleLeave = () => {
+    this.setState({ showSubMenu: false });
+  };
+
+  render() {
+    return (
+      <div className="col-md-8 hidden-sm hidden-xs">
+        <div className="mainmenu">
+          <nav>
+            <ul id="nav">
+              <li>
+                <Link
+                  to="/"
+                  className="menu-item"
+                  activeClassName="menu-item-active"
+                >
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about/"
+                  className="menu-item"
+                  activeClassName="menu-item-active"
+                >
+                  ABOUT US
+                </Link>
+              </li>
+              <li
+                onMouseLeave={this.handleLeave}
+                onMouseEnter={this.handleHover}
+              >
+                <a href="#"> Tours </a>
+                <TransitionGroup>
+                  <CSSTransition
+                    classNames="fade"
+                    timeout={{ enter: 500, exit: 300 }}
+                  >
+                    <div>
+                      {this.state.showSubMenu && (
+                        <SubMenu
+                          tourMenuSections={this.props.tourMenuSections}
+                          handleLeave={this.handleLeave}
+                        />
+                      )}
+                    </div>
+                  </CSSTransition>
+                </TransitionGroup>
+              </li>
+              {this.props.switches.calendarswitch ? (
+                <li>
+                  <Link
+                    to="/tour-calendar/"
+                    className="menu-item"
+                    activeClassName="menu-item-active"
+                  >
+                    Tour Calendar
+                  </Link>
+                </li>
+              ) : null}
+              {this.props.switches.blogswitch ? (
+                <li>
+                  <Link
+                    to="/blog/"
+                    className="menu-item"
+                    activeClassName="menu-item-active"
+                  >
+                    BLOG
+                  </Link>
+                </li>
+              ) : null}
+              <li>
+                <Link
+                  to="/contact/"
+                  className="menu-item"
+                  activeClassName="menu-item-active"
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default class MenuDesktop extends Component {
-    constructor() {
-        super();
-        this.state = { sticky: false }
-        this.onFixedToggle = this.onFixedToggle.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = { sticky: false };
+    this.onFixedToggle = this.onFixedToggle.bind(this);
+  }
 
+  onFixedToggle() {
+    let isSticky = !this.state.sticky;
+    this.setState({ sticky: isSticky });
+  }
 
-    onFixedToggle() {
-        let isSticky = !this.state.sticky;
-        this.setState({ sticky: isSticky })
+  render() {
+    let menuClass;
+    if (this.state.sticky) {
+      menuClass = "header-logo-menu sticky-menu";
+    } else {
+      menuClass = "header-logo-menu";
     }
-
-    render() {
-        let menuClass;
-        if (this.state.sticky) {
-            menuClass = "header-logo-menu sticky-menu";
-        } else {
-            menuClass = "header-logo-menu";
-        }
-        return (
-            <Sticky onFixedToggle={this.onFixedToggle}>
-                <MenuBar
-                    menuClass={menuClass}
-                    tourMenuSections={this.props.sections}
-                    switches={this.props.switches}
-                />
-            </Sticky>
-        )
-    }
+    return (
+      <Sticky onFixedToggle={this.onFixedToggle}>
+        <MenuBar
+          menuClass={menuClass}
+          tourMenuSections={this.props.sections}
+          switches={this.props.switches}
+        />
+      </Sticky>
+    );
+  }
 }
