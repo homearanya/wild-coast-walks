6077f60730877cbe17de37b7c023188b4274cf72
Carlos Gonzalez <homearanya@gmail.com>
2018-12-20 00:52:24 +0100

    remove gatsby plugin offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         icon: "src/assets/img/icon.png" // This path is relative to the root of the site.
       }
     },
-    "gatsby-plugin-offline",
+    `gatsby-plugin-remove-serviceworker`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
