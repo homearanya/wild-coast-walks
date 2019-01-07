1dc2df7e05f6a312a21d666b69ba1e630d946d7f
Carlos Gonzalez <homearanya@gmail.com>
2019-01-07 11:43:24 +0100

    404 page fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,81 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { Link } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "gatsby";
 
-import '../assets/css/404.css'
+import SEO from "../components/SEO/SEO";
+import Layout from "../components/Layout";
 
-export default () => (
-    <div>
-        <Helmet>
-            <meta charSet="utf-8" />
-            <title>Page Not Found - Wild Coast Walks</title>
-        </Helmet>
+import "../assets/css/404.css";
 
-        <div className="error-area">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="error-text-container">
-                            <h1>Oops...</h1>
-                            <h2>Page Not Found!</h2>
-                            <p>Sorry the Page Could not be Found here.<br />
-                                Try using the button below to go to main page/<br />
-                                of the site</p>
-                            <Link to="/">Go to Home</Link>
-                        </div>
-                    </div>
-                </div>
+export default ({ data }) => {
+  const { siteMetadata } = data.siteMetaDataQuery;
+  const { backgroundimage } = data.NotFoundQuery.frontmatter;
+  console.log("backgroundimage", backgroundimage);
+  const postMeta = {
+    title: `Page Not Found - ${siteMetadata.title}`,
+    description: `Page not found. Go back to home page`,
+    slug: "/404/",
+    datePublished: false
+  };
+  return (
+    <Layout>
+      <SEO postData={postMeta} />
+      <div
+        className="error-area"
+        style={{
+          backgroundColor: "#edecec",
+          backgroundImage: `url(${
+            backgroundimage.image.childImageSharp.fluid.src
+          })`,
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "scroll",
+          backgroundPosition: "right top",
+          backgroundSize: "50% 100%"
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="error-text-container">
+                <h1>Oops...</h1>
+                <h2>Page Not Found!</h2>
+                <p>
+                  Sorry the Page Could not be Found here.
+                  <br />
+                  Try using the button below to go to main page
+                  <br />
+                  of the site
+                </p>
+                <Link to="/">Go Home</Link>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-)
+      </div>
+    </Layout>
+  );
+};
+
+export const notFoundPageQuery = graphql`
+  query NotFoundPage {
+    NotFoundQuery: markdownRemark(fields: { slug: { eq: "/notfound/" } }) {
+      frontmatter {
+        backgroundimage {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 785) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          alt
+        }
+      }
+    }
+    siteMetaDataQuery: site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
