d1519a8ce14d67647802b070aac9bdd8f8fefc02
homearanya <homearanya@gmail.com>
2020-04-14 15:43:54 +0200

    update tour thumbnails on home page

--- src/components/TourThumbnail.js
+++ src/components/TourThumbnail.js
@@ -1,60 +1,58 @@
-import React from "react";
-import { Link } from "gatsby";
-import Img from "gatsby-image";
+import React from "react"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
 
-const TourThumbnail = props => {
+const TourThumbnail = (props) => {
   if (!props.tourDetails) {
-    return null;
+    return null
   }
-  const tourSlug = props.tourDetails.fields.slug;
-  const tourDetails = props.tourDetails.frontmatter;
+  const tourSlug = props.tourDetails.fields.slug
+  const tourDetails = props.tourDetails.frontmatter
   return (
     <div className="col-md-4 col-sm-6 col-xs-12">
       <div className="single-adventure">
-        <Link to={tourSlug}>
-          {tourDetails.imagethumbnail.image ? (
-            tourDetails.imagethumbnail.image.childImageSharp ? (
-              <Img
-                fluid={tourDetails.imagethumbnail.image.childImageSharp.fluid}
-                alt={tourDetails.imagethumbnail.alt}
-              />
-            ) : (
-              <img
-                src={tourDetails.imagethumbnail.image}
-                alt={tourDetails.imagethumbnail.alt}
-              />
-            )
-          ) : null}
-        </Link>
+        {tourDetails.imagethumbnail.image ? (
+          tourDetails.imagethumbnail.image.childImageSharp ? (
+            <Img
+              fluid={tourDetails.imagethumbnail.image.childImageSharp.fluid}
+              alt={tourDetails.imagethumbnail.alt}
+            />
+          ) : (
+            <img
+              src={tourDetails.imagethumbnail.image}
+              alt={tourDetails.imagethumbnail.alt}
+            />
+          )
+        ) : null}
         <div className="adventure-text effect-bottom">
-          <Link to={tourSlug}>
-            <div className="transparent-overlay">
-              <h4>
-                {tourDetails.tour_id} | <span>{tourDetails.destination}</span>
-              </h4>
-              <span className="trip-time">
-                <i className="fa fa-clock-o" />
-                {tourDetails.duration}
-              </span>
-              <p>
-                {tourDetails.shortdescription.length > 233
-                  ? tourDetails.shortdescription.substring(0, 230) + "..."
-                  : tourDetails.shortdescription}
-              </p>
+          <div className="transparent-overlay">
+            <h4>
+              {tourDetails.tour_id} | <span>{tourDetails.destination}</span>
+            </h4>
+            <span className="trip-time">
+              <i className="fa fa-clock-o" />
+              {tourDetails.duration}
+            </span>
+            <p>
+              {tourDetails.shortdescription.length > 233
+                ? tourDetails.shortdescription.substring(0, 230) + "..."
+                : tourDetails.shortdescription}
+            </p>
+          </div>
+          <div className="adventure-price-link">
+            <div class="adventure-price">
+              <div className="trip-person">
+                From <span>{tourDetails.price}</span> per person
+              </div>
             </div>
-            <div className="adventure-price-link">
-              <span className="trip-person">From</span>
-              <span className="trip-person">
-                <span>{tourDetails.price}</span>
-              </span>
-              <span className="trip-person">per person</span>
-              <span className="trip-price">&nbsp;</span>
-            </div>
-          </Link>
+            <Link className="adventure-link" to={tourSlug}>
+              Learn more
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TourThumbnail;
+export default TourThumbnail
